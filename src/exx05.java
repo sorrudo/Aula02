4fda663f5d31d8a2a9d5e365c057832195562376
Guilherme Radan <radanguilherme@hotmail.com>
2021-06-29 09:46:57 -0300

    implementando exx05 e exx06

---
+++ src/exx05.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class exx05 {
+    public static void main(String[] args) {
+        /*Um drone de limpeza de ruas deve escolher carregar sempre o lixo de menor peso.
+         Entre os pesos de 3 lixos informados pelo usuário, um programa deve indicar qual
+         deles deve ser carregado pelo drone. */
+        Scanner leitor = new Scanner(System.in);
+        int peso1;
+        int peso2;
+        int peso3;
+        System.out.println("Digite o valor do primeiro peso");
+        peso1 = leitor.nextInt();
+        System.out.println("Digite o valor do segundo peso");
+        peso2 = leitor.nextInt();
+        System.out.println("Digite o valor do terceiro peso");
+        peso3 = leitor.nextInt();
+        if (peso1 < peso2 && peso1 < peso3) {
+            System.out.println("O Drone vai carregar primeiro o 1° peso indicado");
+        }else {
+            if (peso2 < peso1 && peso2 < peso3) {
+                System.out.println("O Drone vai carregar primeiro o 2° peso indicado");
+            }else {
+                System.out.println("O Drone vai carregar primeiro o 3° peso indicado");
+            }
+        }
+    }
+}
---
+++ src/exx06.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class exx06 {
+    public static void main(String[] args) {
+        /*Uma   loja   oferece   um   desconto   de   3%   aos   clientes
+          que   tiverem   o   cupom “DIADEFESTA”. Faça um programa onde o
+          funcionário digite o valor da compra e um cupom, informando o valor final a ser pago.
+         */
+        Scanner leitor = new Scanner(System.in);
+        double valor;
+        String cupom;
+        System.out.println("Digite o valor do produto");
+        valor = leitor.nextDouble();
+        System.out.println("Digite o nome do cupom se tiver");
+        cupom = leitor.next();
+        if ( cupom.equalsIgnoreCase( "DIADEFESTA")) {
+            valor = valor * 0.97;
+            System.out.println("O seu produto com desconto fica R$ " + valor + " reais");
+        }else {
+            System.out.println("Seu cupon não exite, o valor do produto fica R$ " + valor + " reais");
+        }
+    }
+}
